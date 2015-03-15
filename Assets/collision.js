d3f3e2f5cbe4086237f4243b35cae1949e82ea61
Ssteve85uk <stephen.mangold@knights.ucf.edu>
2015-03-14 23:51:17 -0400

    new
    new

--- Assets/collision.js
+++ Assets/collision.js
@@ -1,16 +1,1 @@
-﻿#pragma strict
-
-var damage : int = 50;
-var distance : float;
-
-function update()
-{
-if (Input.GetMouseButtonDown("fire1")
-{
-var hit : RaycastHit;
-if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward),hit))
-{
-distance = hit.distance;
-hit.transform.SendMessage("Apply Damage"),damage, SendMessageOptions.DontRequireReceiver);
-}
-}
+﻿
